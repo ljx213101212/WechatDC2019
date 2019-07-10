56da7c4f1f2e0b9951d95025c0f63a2dc2affaa1
ljx213101212 <lijixiang.nus@gmail.com>
2019-07-11 00:42:45 +0800

    Add eventDetail entry element 1. Utils - dummy data from EventService.js 2. Render dummydata in a listview in home page. 3. onTap to navigate to eventDetail page.

---
+++ utils/dummy_data/dummyRecentEvent.js
@@ -0,0 +1,48 @@
+module.exports = [ 
+  {
+    "activeId": 1000,
+    "activeImg": "https://dummyimage.com/40x40/000000/ffffff.png", // 活动图片
+    "activeTitle": "人工智能", // 活动主题
+    "activeSubTitle": "智慧生活",
+    "activeState": "这是啥",
+    "activeSignUpStartTime": "2019-07-16T18:25:43.511Z",
+    "activeSignUpEndTime": "2019-07-19T18:25:43.511Z",
+    "activeStartTime": "2019-07-23T18:25:43.511Z",
+    "activeEndTime": "2019-07-23T20: 25: 43.511Z",
+    "activeAddress": "这是活动地址",
+    "activeContent": "这是活动内容",
+    "createAt": "2019-07-13T20: 25: 43.511Z",
+    "updateAt": "2019-07-14T20: 25: 43.511Z"
+  },
+  {
+    "activeId": 1001,
+    "activeImg": "https://dummyimage.com/40x40/000000/ffffff.png", // 活动图片
+    "activeTitle": "人工智能", // 活动主题
+    "activeSubTitle": "智慧生活",
+    "activeState": "这是啥",
+    "activeSignUpStartTime": "2019-07-16T18:25:43.511Z",
+    "activeSignUpEndTime": "2019-07-19T18:25:43.511Z",
+    "activeStartTime": "2019-07-23T18:25:43.511Z",
+    "activeEndTime": "2019-07-23T20: 25: 43.511Z",
+    "activeAddress": "这是活动地址",
+    "activeContent": "这是活动内容",
+    "createAt": "2019-07-13T20: 25: 43.511Z",
+    "updateAt": "2019-07-14T20: 25: 43.511Z"
+  },
+    {
+      "activeId": 1002,
+      "activeImg": "https://dummyimage.com/40x40/000000/ffffff.png", // 活动图片
+      "activeTitle": "人工智能", // 活动主题
+      "activeSubTitle": "智慧生活",
+      "activeState": "这是啥",
+      "activeSignUpStartTime": "2019-07-16T18:25:43.511Z",
+      "activeSignUpEndTime": "2019-07-19T18:25:43.511Z",
+      "activeStartTime": "2019-07-23T18:25:43.511Z",
+      "activeEndTime": "2019-07-23T20: 25: 43.511Z",
+      "activeAddress": "这是活动地址",
+      "activeContent": "这是活动内容",
+      "createAt": "2019-07-13T20: 25: 43.511Z",
+      "updateAt": "2019-07-14T20: 25: 43.511Z"
+    }
+
+]
