f604dd0eae91248085ef2f1ae03be080b3a400d9
ljx213101212 <lijixiang.nus@gmail.com>
2019-07-14 16:47:44 +0800

    Add buyTicketComponent and purchasedTicketComponent.js

---
+++ pages/eventDetail/components/buyTicket/buyTicketComponent.js
@@ -0,0 +1,31 @@
+// pages/eventDetail/components/buyTicket/buyTicketComponent.js
+Component({
+  
+  /**
+   * @see https://developers.weixin.qq.com/miniprogram/dev/framework/custom-component/wxml-wxss.html
+   */
+  options: {
+    addGlobalClass: true,
+  },
+  /**
+   * Component properties
+   */
+  
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
