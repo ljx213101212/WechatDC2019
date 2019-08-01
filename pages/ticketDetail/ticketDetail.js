2117c467765eb2c0da32f4ef5e6dff353604a6eb
Lu Angxiao <luangxiao914@gmail.com>
2019-08-02 03:54:45 +0800

    finish ticketDetail

--- pages/ticketDetail/ticketDetail.js
+++ pages/ticketDetail/ticketDetail.js
@@ -13,8 +13,35 @@ Page({
    */
   onLoad: function (options) {
     var openid = wx.getStorageSync('openid');
-    var eventid = 'eventid';
-    var qrCodeUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + openid + '*' + eventid;
+    //先显示页面加载
+    wx.showLoading({
+      title: '加载中 Loading',
+    });
+    //打印传递数据
+    console.log(options.eventId);
+    //读取全局缓存拉current event 的snapshot.
+    let eventOriginPassenger = wx.getStorageSync('eventOriginSnapshotJsonStr');
+
+    //页面数据parsing
+    let eventOrigin = {};
+    try {
+      eventOrigin = JSON.parse(eventOriginPassenger);
+      
+      eventOrigin.startDate = new Date(eventOrigin.startDate).toDateString();
+      
+      //设置页面数据
+      this.setData({
+        event: eventOrigin,
+        eventId: options.eventId
+      }, () => {
+        //callback 取消页面加载
+        wx.hideLoading();
+      });
+    } catch (e) {
+      console.log(e);
+      wx.hideLoading();
+    }
+    var qrCodeUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + openid + '*' + this.data.eventId;
     this.setData({
       qrCodeUrl: qrCodeUrl
     })
